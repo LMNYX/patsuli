5acf013553ce80c0ed3727cf0bd85350c2ba26f0
LMNYX <me@reze.moe>
2025-07-13 04:08:08 +0500

    docs: update readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # patsuli
 
 ![patchouli knowledge](https://uwu.so/eemLTBvTZOe)
+
+Development Stage: **pre-pre-alpha indev**
+
+Refer to [Discord Server](https://discord.gg/n9ysBD6G6Q) for updates
--- pages/library.vue
+++ pages/library.vue
@@ -6,7 +6,6 @@
       <Card title="Eromanga Sensei" cover="https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/nx99026-5Eg650WAd9Rj.jpg" @mousemove="onMouseMove" @mouseleave="onMouseLeave" />
       <Card title="Kami no Tou" cover="https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx113322-gwV3eakaVqZQ.png" @mousemove="onMouseMove" @mouseleave="onMouseLeave" />
       <CardNewElement @mousemove="onMouseMove" @mouseleave="onMouseLeave" />
-
     </div>
   </div>
 </template>
